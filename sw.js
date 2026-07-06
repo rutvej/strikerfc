93d7e0ad1509ef1666d1958bf80161e2aea6e903
rutvej <waghela.rutvej1@gmail.com>
2026-07-06 20:29:07 +0000

    Fix: Update Service Worker fetch handler to support caching of CORS static resources (Google Fonts, CDN scripts)

--- sw.js
+++ sw.js
@@ -53,7 +53,7 @@ self.addEventListener('fetch', event => {
         }
 
         return fetch(event.request).then(response => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
+          if (!response || response.status !== 200 || (response.type !== 'basic' && response.type !== 'cors')) {
             return response;
           }
           const responseToCache = response.clone();
